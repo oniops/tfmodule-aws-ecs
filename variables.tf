2d39af60d8e28f06671a141a2e466aa43aae0fab
seonbo.shim <seonbo.shim@bespinglobal.com>
2022-08-22 16:59:35 +0900

    ECS 클러스터 구성 보완

--- variables.tf
+++ variables.tf
@@ -13,13 +13,18 @@ variable "cluster_name" {
 variable "capacity_providers" {
   description = "List of short names of one or more capacity providers to associate with the cluster. Valid values also include FARGATE and FARGATE_SPOT."
   type        = list(string)
-  default     = []
+  default     = ["FARGATE", "FARGATE_SPOT"]
 }
 
 variable "default_capacity_provider_strategy" {
   description = "The capacity provider strategy to use by default for the cluster. Can be one or more."
   type        = list(map(any))
-  default     = []
+  default     = [
+    {
+      capacity_provider = "FARGATE"
+      weight            = 1
+    }
+  ]
 }
 
 variable "container_insights" {
